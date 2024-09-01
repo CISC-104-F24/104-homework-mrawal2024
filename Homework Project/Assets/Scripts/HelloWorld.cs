e4a35a54d1fbfad0f7f049efb770d4a719f5b657
mrawal2024 <mrawal@udel.edu>
2024-09-01 11:31:28 -0400

    Finished Homework 0

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -8,6 +8,11 @@ public class HelloWorld : MonoBehaviour
     void Start()
     {
      Debug.Log("Hello, world!");
+        Debug.LogWarning("Hello, I am a warning!");
+        Debug.LogError("Player Error!!");
+        //Author Maharshi Rawal
+        /*A game idea I have is a horror survival game of the player being trapped in school at night while a monster lurks the entire school and you have to escape and I would want to basically learn ai movement and setting up the environment.
+         */
     }
 
     // Update is called once per frame
